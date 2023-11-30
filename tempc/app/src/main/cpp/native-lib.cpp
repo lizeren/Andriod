33a7cfcedd86b19e49b09fddde3674770383f80b
unknown <li3103@purdue.edu>
2023-11-30 09:08:26 -0500

    mem/ins barrier added

--- tempc/app/src/main/cpp/native-lib.cpp
+++ tempc/app/src/main/cpp/native-lib.cpp
@@ -13,7 +13,6 @@
 
 //function to be called in java
 
-uint8_t array[10*4096];
 
 
 uint64_t
@@ -38,50 +37,28 @@ perf_get_timing(libflush_session_t* session)
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_tempc_MainActivity_stringFromJNI(JNIEnv* env,jobject /* this */) {
-	std::string hello = "Hello from pure C++ accessed by Java";
-	return env->NewStringUTF(hello.c_str());
+    std::string hello = "Hello from pure C++ accessed by Java";
+    return env->NewStringUTF(hello.c_str());
 }
 // use method from java
 extern "C" JNIEXPORT jstring JNICALL Java_com_example_tempc_MainActivity_process(JNIEnv* env,jobject mainActivityInst) {
-	const jclass mainActivityCls = env->GetObjectClass(mainActivityInst);
-	const jmethodID methodid = env->GetMethodID(mainActivityCls,"processInJava","()Ljava/lang/String;");
+    const jclass mainActivityCls = env->GetObjectClass(mainActivityInst);
+    const jmethodID methodid = env->GetMethodID(mainActivityCls,"processInJava","()Ljava/lang/String;");
 
-	if(methodid == nullptr){
-		return env->NewStringUTF("");
-	}
+    if(methodid == nullptr){
+        return env->NewStringUTF("");
+    }
     const jobject result = env->CallObjectMethod(mainActivityInst, methodid);
-	const std::string java_msg = env->GetStringUTFChars((jstring) result, JNI_FALSE);
+    const std::string java_msg = env->GetStringUTFChars((jstring) result, JNI_FALSE);
 
     const std::string c_msg = "result from Java =>";
 
-    //test();
-/*
-    libflush_session_t* libflush_session;
-    if (libflush_init(&libflush_session, NULL) == false) {
 
-    }
-    uint64_t time1_mono = get_monotonic_time();
-    //sleep(1);
-    uint64_t time2_mono = get_monotonic_time();
-    uint64_t diff_mono = time2_mono - time1_mono;
-    const std::string time_string_mono = std::to_string(diff_mono);
-
-
-    uint64_t time1_perf = perf_get_timing(libflush_session);
-    uint64_t time2_perf = perf_get_timing(libflush_session);
-    uint64_t diff_perf = time2_perf - time1_perf;
-    const std::string time_string_perf = std::to_string(diff_perf);
-
-
-    const std::string msg = c_msg + java_msg
-            + "\nMonotonic timer diff:" +time_string_mono +
-            + "\nPerf timer diff:"+ time_string_perf +
-            "\ntemp value:"+ temp_disp_string;
-*/
-
+    // timer
 
     arm_v8_timing_init();
     int64_t result_pmccntr = arm_v8_get_timing();
+    asm volatile ("isb"); // Serialize after reading the counter
 
     int64_t temp_disp1,temp_disp2,diff;
     int32_t freq;
@@ -94,33 +71,96 @@ extern "C" JNIEXPORT jstring JNICALL Java_com_example_tempc_MainActivity_process
     const std::string show1 = std::to_string(temp_disp1);
     const std::string show2 = std::to_string(temp_disp2);
     const std::string show_freq = std::to_string(freq);
-    const std::string show_result_pmccntr = std::to_string(result_pmccntr);
+    int64_t result_pmccntr2 = arm_v8_get_timing();
+
+    const std::string show_result_pmccntr = std::to_string(result_pmccntr2 - result_pmccntr);
 
 
+    // cache flushing
+//    uint8_t array[10*8192];
+//    int temp,temp2;
+//    int i;
+//    // Initialize the array
+//    for(i=0; i<10; i++) array[i*8192]=1;
+//
+//    // FLUSH the array from the CPU cache
+//    for(i=0; i<10; i++) {
+//        asm volatile ("dc civac, %0" : : "r" (&array[i*8192]) : "memory");
+//    }
+//
+//    // Access some of the array items
+//    array[7*8192] = 200;
+//
+//    int64_t fast_diff1, fast_diff2;
+//    asm volatile("MRS %0, cntvct_el0 " : "=r" (fast_diff1));
+//    temp2 = array[7*8192];
+//    asm volatile("MRS %0, cntvct_el0 " : "=r" (fast_diff2));
+//    const std::string fast_diff = std::to_string(fast_diff2 - fast_diff1);
+//
+//
+//    int64_t slow_diff1, slow_diff2;
+//    asm volatile("MRS %0, cntvct_el0 " : "=r" (slow_diff1));
+//    temp = array[1*8192];
+//    asm volatile("MRS %0, cntvct_el0 " : "=r" (slow_diff2));
+//    const std::string slow_diff = std::to_string(slow_diff2 - slow_diff1);
 
-    int temp,temp2;
-    array[3*4096] = 100;
-    array[7*4096] = 200;
-
+    uint8_t array[10 * 8192];
+    volatile uint8_t *volatile_array = array; // Declare a volatile pointer to the array
+    int temp, temp2, i;
+    int64_t fast_diff1, fast_diff2;
     int64_t slow_diff1, slow_diff2;
-    asm volatile("MRS %0, cntvct_el0 " : "=r" (slow_diff1));
-    temp = array[1*4096];
-    asm volatile("MRS %0, cntvct_el0 " : "=r" (slow_diff2));
+
+// Initialize the array
+    for (i = 0; i < 10; i++) {
+        volatile_array[i * 8192] = 1;
+        asm volatile ("dsb ish"); // Data synchronization barrier ensure write has completed
+    }
+
+// FLUSH the array from the CPU cache
+    for (i = 0; i < 10; i++) {
+        asm volatile ("dc civac, %0" : : "r" (&volatile_array[i * 8192]) : "memory");
+        //asm volatile ("dsb ish"); // Data synchronization barrier ensure invalidation has completed
+        asm volatile ("isb"); // Insert isb for serialization after cache flush
+    }
+
+// Access some of the array items
+    volatile_array[7 * 8192] = 200;
+
+
+// Timestamp and array access with serialization and data barriers
+    asm volatile ("isb"); // Serialize before reading the counter
+    asm volatile ("MRS %0, cntvct_el0" : "=r" (fast_diff1));
+    asm volatile ("isb"); // Serialize after reading the counter
+    temp2 = volatile_array[7 * 8192];
+    asm volatile ("dsb ish"); // Data synchronization barrier after volatile access
+    asm volatile ("isb"); // Serialize before reading the counter again
+    asm volatile ("MRS %0, cntvct_el0" : "=r" (fast_diff2));
+    asm volatile ("isb"); // Serialize after reading the counter
+    const std::string fast_diff = std::to_string(fast_diff2 - fast_diff1);
+
+
+// Similar pattern for the second measurement
+    asm volatile ("isb");
+    asm volatile ("MRS %0, cntvct_el0" : "=r" (slow_diff1));
+    asm volatile ("isb");
+    temp = volatile_array[1 * 8192];
+    asm volatile ("dsb ish");
+    asm volatile ("isb");
+    asm volatile ("MRS %0, cntvct_el0" : "=r" (slow_diff2));
+    asm volatile ("isb");
     const std::string slow_diff = std::to_string(slow_diff2 - slow_diff1);
 
 
-    int i;
-    for(i=0; i<10; i++) array[i*4096]=1;
-    for(i=0; i<10; i++) {
-        asm volatile ("dc civac, %0" : : "r" (array) : "memory");
-    }
 
 
-    int64_t fast_diff1, fast_diff2;
-    asm volatile("MRS %0, cntvct_el0 " : "=r" (fast_diff1));
-    temp2 = array[3*4096];
-    asm volatile("MRS %0, cntvct_el0 " : "=r" (fast_diff2));
-    const std::string fast_diff = std::to_string(fast_diff2 - fast_diff1);
+
+
+
+
+
+
+
+
 
     const std::string msg = c_msg + java_msg  +
                             "\ndiff value:"+ temp_disp_string +
@@ -131,10 +171,7 @@ extern "C" JNIEXPORT jstring JNICALL Java_com_example_tempc_MainActivity_process
                             "\nslow:" + slow_diff +
                             "\nfast:" + fast_diff ;
 
-    // Use libflush...
-    // Terminate libflush
-    //libflush_terminate(libflush_session);
 
 
-	return env->NewStringUTF(msg.c_str());
+    return env->NewStringUTF(msg.c_str());
 }
